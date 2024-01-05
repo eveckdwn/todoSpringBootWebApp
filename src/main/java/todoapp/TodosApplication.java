7400ba65b49705ee8da13d24c57ea423c92b8c47
changjoosohn <eveckdwn@gmail.com>
2024-01-06 03:51:07 +0900

    스프링 MVC 정적 자원 설정하고 연결하기

--- src/main/java/todoapp/TodosApplication.java
+++ src/main/java/todoapp/TodosApplication.java
@@ -2,12 +2,16 @@ package todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import todoapp.web.WebMvcConfiguration;
 
 @SpringBootApplication
+//  @ComponentScan("todoapp")
+//  @SpringBootApplication이(가) 이미 지정된 @ComponentScan을(를) 적용했습니다.
 public class TodosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TodosApplication.class, args);
     }
-
 }
--- src/main/java/todoapp/web/WebMvcConfiguration.java
+++ src/main/java/todoapp/web/WebMvcConfiguration.java
@@ -1,5 +1,9 @@
 package todoapp.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
@@ -9,7 +13,32 @@ import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
  *
  * @author springrunner.kr@gmail.com
  */
+@Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    public WebMvcConfiguration() {
+        logger.debug("스프링 MVC 설정자가 생성됩니다.");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //  리소스 핸들러를 등록, 정적 자원을 처리할 수 있다.
+
+        //  http://localhost:8080/assets/css/todos.css
+
+        //  서블릿 컨텍스트 경로에서 정적 자원 제공
+        //  registry.addResourceHandler("/assets/**").addResourceLocations("assets/");
+
+        //  파일 경로에서 정적 자원 제공
+        //  registry.addResourceHandler("/assets/**").addResourceLocations("file:/Users/changjoosohn/IdeaProjects/todos/files/assets/");
+
+        //  클래스패스 경로에서 정적 자원 제공
+        //  registry.addResourceHandler("/assets/**").addResourceLocations("classpath:assets/");
+
+        registry.addResourceHandler("/assets/**").addResourceLocations("assets/", "file:/Users/changjoosohn/IdeaProjects/todos/files/assets/", "classpath:assets/");
+    }
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
